33be57d6ab1eeb822b645cde3b042cf02f7cdf3e
bruno pessoa nunes de melo <brunopessoa.melo@gmail.com>
2021-05-23 16:27:58 -0300

    adicionando as variáveis que irão se referir a cada label dos inputs forms.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("Olá mundo")
+// criando as variaveis que irão se referir a cada label dos form 
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelector("#exampleGenderM");
+var gender = document.querySelector("#exampleInputGenderF");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail1");
+var password = document.querySelector("#exampleInputPassword1");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.querySelector("#");
